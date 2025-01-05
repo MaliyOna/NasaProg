d1e95e19eefa34290396a7450aca7664d930b463
MaliyOna <veronika.pr777@gmail.com>
2025-01-05 21:40:45 +0300

    create first migration

--- NasaPet.DAL/ApplicationDbContext.cs
+++ NasaPet.DAL/ApplicationDbContext.cs
@@ -4,4 +4,11 @@ namespace NasaPet.DAL;
 
 public class ApplicationDbContext : DbContext
 {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies;Integrated Security=True;Connect Timeout=30;Encrypt=False;");
+        }
+    }
 }
